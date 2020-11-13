7f50402e5dea49c09258439c669816fab2a44576
Maylo <camilo.iegamar.z@gmail.com>
2020-11-13 15:05:07 -0500

    version formulario

---
+++ src/components/pages/SignUpHere/SignHere.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './SignUpHere';
+import registerServiceWorker from './Data';
+
+function SignHere (){
+
+ <App />
+ registerServiceWorker 
+  
+}
+
+export default SignHere;
